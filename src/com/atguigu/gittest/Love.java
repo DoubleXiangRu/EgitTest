05a90faa75bfcab4b286c03b81a360702b9b3913
XiangRuQian163 <alonexrq@163.com>
2020-02-25 21:44:57 +0800

    愿世界安好！！！！

--- src/com/atguigu/gittest/Love.java
+++ src/com/atguigu/gittest/Love.java
@@ -7,6 +7,8 @@ public class Love {
 		System.out.println("其实最后有没有和最爱的人在一起也没有多大的事！！！");
 		System.out.println("人生最幸福的事：和一个爱自己的人，走下去！！！");
 		System.out.println("渐行渐远渐无书，水阔鱼沉何处问！！！！");
+		
+		System.out.println("愿世界安好！！！！！");
 	}
 
 }
