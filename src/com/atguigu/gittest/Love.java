b911036e9b1698fa34b190e023d8ae62b2ad9af2
DoubleXiangRu <1171899614@qq.com>
2020-02-25 20:56:40 +0800

    其实最后有没有和最爱的人在一起也没有多大的事！！！

---
+++ src/com/atguigu/gittest/Love.java
@@ -0,0 +1,12 @@
+package com.atguigu.gittest;
+
+public class Love {
+
+	public static void main(String[] args) {
+		System.out.println("我喜欢你！！！！");
+		System.out.println("其实最后有没有和最爱的人在一起也没有多大的事！！！");
+		System.out.println("人生最幸福的事：和一个爱自己的人，走下去！！！");
+		System.out.println("渐行渐远渐无书，水阔鱼沉何处问！！！！");
+	}
+
+}
